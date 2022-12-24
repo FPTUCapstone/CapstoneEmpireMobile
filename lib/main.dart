74f11e96423e42fbed445b1cfe6dac4b9025e083
hieutrung0510 <hieutrung0510@gmail.com>
2022-12-25 00:51:58 +0700

    [TrungTDH] Move file user_profile into screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:empiregarage_mobile/routes/routes.dart';
 import 'package:empiregarage_mobile/screens/login/login_screen.dart';
 import 'package:empiregarage_mobile/screens/login/otp_confirmation.dart';
 import 'package:empiregarage_mobile/screens/welcome/welcome_screen.dart';
-import 'package:empiregarage_mobile/user_profile/profile.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
