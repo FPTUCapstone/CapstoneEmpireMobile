a923c498107c9081def6cd6a4ef27bf4758b4a5b
hieutrung0510 <hieutrung0510@gmail.com>
2023-01-10 22:50:45 +0700

    [TrungTDH] Change HomePage ListView.builder code

--- lib/application_layer/screens/home_page/home_page.dart
+++ lib/application_layer/screens/home_page/home_page.dart
@@ -182,14 +182,14 @@ class _HomePageState extends State<HomePage> {
                     height: 20.h,
                   ),
 
-                  Container(
-                    child: ListView.builder(
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container();
-                      },
-
-                    ),
-                  ),
+                  // Container(
+                  //   child: ListView.builder(
+                  //     itemBuilder: (BuildContext context, int index) {
+                  //       return Container();
+                  //     },
+                  //
+                  //   ),
+                  // ),
                 ],
               ),
             ),
