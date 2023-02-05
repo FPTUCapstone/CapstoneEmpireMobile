925a3813b0e13d14affe42d25f632babf512070a
hieutrung0510 <hieutrung0510@gmail.com>
2023-02-06 01:53:44 +0700

    [TrungTDH] Done Service Details and Move to Zalo 6/2/23

--- lib/application_layer/screens/services/service_details.dart
+++ lib/application_layer/screens/services/service_details.dart
@@ -12,140 +12,249 @@ class ServiceDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          width: 375.w,
-          height: 352.h,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/image/service-picture/service-picture2.png'),
-            )
-          ),
-          child: SafeArea(
-            child: Padding(
-              padding: EdgeInsets.only(top: 24),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      IconButton(
-                          onPressed: (){
-                            Navigator.pop(context);
-                          },
-                          icon: Icon(Icons.arrow_back_ios_new_outlined),
-                          color: AppColors.whiteButtonColor,
-                          )
-                    ],
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 300.h,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage("assets/image/service-picture/service-picture2.png"),
                   )
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    child: SafeArea(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            child: SafeArea(
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    margin: const EdgeInsets.symmetric(horizontal: 12),
+                                    child: Row(
+                                      children: <Widget>[
+                                        SizedBox(height: 100.h,),
+                                        Container(
+                                          decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(100),
+                                              shape: BoxShape.rectangle,
+                                              boxShadow: [
+                                                BoxShadow(
+                                                  offset: Offset(0,1),
+                                                  blurRadius: 20,
+                                                  color: AppColors.unselectedBtn,
+                                                )
+                                              ]
+                                          ),
+                                          child: IconButton(
+                                              onPressed: (){
+                                                //TODO
+                                              },
+                                              icon: const Icon(
+                                                Icons.arrow_back_outlined,
+                                                color: AppColors.whiteButtonColor,
+                                              )),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(height: 100.h,),
+                                  Container(
+                                    margin: const EdgeInsets.symmetric(horizontal: 24),
+                                    child: Align(
+                                      alignment: Alignment.bottomLeft,
+                                      child: Text(
+                                        "Bảo dưỡng định kì",
+                                        style:TextStyle(
+                                          fontFamily: 'SFProDisplay',
+                                          fontSize: 30.sp,
+                                          fontWeight: FontWeight.w600,
+                                          color: AppColors.whiteTextColor,
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Card(
+                              semanticContainer: true,
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              elevation: 5,
+                              margin: EdgeInsets.all(10),
+                              child: Container(
+                                color: Colors.deepOrange,
+                              ),
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ),
+
                 ],
               ),
             ),
-          ),
+          ],
         ),
         bottomSheet: ClipRRect(
           borderRadius: BorderRadius.circular(10),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.w),
-            child: Container(
-                height: 390.h,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            'Description',
-                            style:TextStyle(
-                                fontSize: 18.sp,
-                                fontWeight: FontWeight.w900,
-                                color: AppColors.blackTextColor,
-                                fontFamily: 'SFProDisplay'),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      Container(
-                        width: 343.w,
-                        height: 100.h,
-                        child: Column(
-                          children: [
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: ReadMoreText(
-                                'We are a family owned auto repair service center which offers complete automotive repairs on all makes and models of vehicles. Our technicians are ASE Certified and Washington State Emissions Certified. We have ….read more',
-                                style: TextStyle(
-                                  fontSize: 13.sp,
-                                  fontWeight: FontWeight.w600,
-                                  color: AppColors.blackTextColor,
-                                  fontFamily: 'SFProDisplay',
-                                ),
-                                trimLines: 3,
-                                colorClickableText: AppColors.blueTextColor,
-                                trimMode: TrimMode.Line,
-                                trimCollapsedText: 'Read more',
-                                trimExpandedText: 'Show less',
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            'Reviews',
-                            style:TextStyle(
-                                fontSize: 17.sp,
-                                fontWeight: FontWeight.w900,
-                                color: AppColors.blackTextColor,
-                                fontFamily: 'SFProDisplay'),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      Container(
-                        width: 343.w,
-                        height: 100.h,
-                        child: Column(
-                          children: [
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: ReadMoreText(
-                                'We are a family owned auto repair service center which offers complete automotive repairs on all makes and models of vehicles. Our technicians are ASE Certified and Washington State Emissions Certified. We have ….read more',
-                                style: TextStyle(
-                                  fontSize: 13.sp,
-                                  fontWeight: FontWeight.w600,
-                                  color: AppColors.blackTextColor,
-                                  fontFamily: 'SFProDisplay',
-                                ),
-                                trimLines: 3,
-                                colorClickableText: AppColors.blueTextColor,
-                                trimMode: TrimMode.Line,
-                                trimCollapsedText: 'Read more',
-                                trimExpandedText: 'Show less',
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40.h,
-                      ),
-                      MoveToAnotherApp(),
-                    ],
-
-                  ),
+          child: Container(
+            decoration: BoxDecoration(
+                color: AppColors.whiteTextColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15.r),
+                  topRight: Radius.circular(15.r),
                 ),
+                boxShadow: const [
+                  BoxShadow(
+                    offset: Offset(0,1),
+                    blurRadius: 50,
+                    color: AppColors.unselectedBtn,
+                  )
+                ]
+            ),
+            height: 390.h,
+            child: SingleChildScrollView(
+              reverse: true,
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Text(
+                            "182 lượt đặt",
+                          style:TextStyle(
+                            fontFamily: 'SFProDisplay',
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.w900,
+                            color: AppColors.blackTextColor,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          "20.000",
+                          style: TextStyle(
+                              fontSize: 15.sp,
+                              fontWeight: FontWeight.w600,
+                              color: AppColors.greenTextColor,
+                              fontFamily: 'SFProDisplay'),
+                        ),
+
+                      ],
+
+                    ),
+                    Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                    ),
+                    SizedBox(height: 10.h,),
+                    Row(
+                      children: [
+                        Text(
+                          'Mô tả chi tiết',
+                          style:TextStyle(
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.w900,
+                              color: AppColors.blackTextColor,
+                              fontFamily: 'SFProDisplay'),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Container(
+                      width: 343.w,
+                      height: 80.h,
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: ReadMoreText(
+                              'We are a family owned auto repair service center which offers complete automotive repairs on all makes and models of vehicles. Our technicians are ASE Certified and Washington State Emissions Certified. We have ….read more',
+                              style: TextStyle(
+                                fontSize: 13.sp,
+                                fontWeight: FontWeight.w600,
+                                color: AppColors.blackTextColor,
+                                fontFamily: 'SFProDisplay',
+                              ),
+                              trimLines: 3,
+                              colorClickableText: AppColors.blueTextColor,
+                              trimMode: TrimMode.Line,
+                              trimCollapsedText: 'Read more',
+                              trimExpandedText: 'Show less',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Đánh giá',
+                          style:TextStyle(
+                              fontSize: 17.sp,
+                              fontWeight: FontWeight.w900,
+                              color: AppColors.blackTextColor,
+                              fontFamily: 'SFProDisplay'),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Container(
+                      width: 343.w,
+                      height: 80.h,
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: ReadMoreText(
+                              'We are a family owned auto repair service center which offers complete automotive repairs on all makes and models of vehicles. Our technicians are ASE Certified and Washington State Emissions Certified. We have ….read more',
+                              style: TextStyle(
+                                fontSize: 13.sp,
+                                fontWeight: FontWeight.w600,
+                                color: AppColors.blackTextColor,
+                                fontFamily: 'SFProDisplay',
+                              ),
+                              trimLines: 3,
+                              colorClickableText: AppColors.blueTextColor,
+                              trimMode: TrimMode.Line,
+                              trimCollapsedText: 'Read more',
+                              trimExpandedText: 'Show less',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40.h,
+                    ),
+                    const MoveToAnotherApp(),
+                    SizedBox(
+                      height: 40.h,
+                    ),
+                  ],
+
+                ),
+              ),
             ),
           ),
         ),
--- lib/application_layer/widgets/move_to_another_app.dart
+++ lib/application_layer/widgets/move_to_another_app.dart
@@ -14,71 +14,128 @@ class MoveToAnotherApp extends StatelessWidget {
       children: <Widget>[
         Expanded(
           child: ElevatedButton(
-            onPressed: () => showDialog<String>(
-              context: context,
-              builder: (BuildContext context) => AlertDialog(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-                title: Text(
-                  'Truy cập ứng dụng Zalo',
-                  style:TextStyle(
-                      fontSize: 18.sp,
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.blackTextColor,
-                      fontFamily: 'SFProDisplay'),
-                ),
-                content: Text(
-                  'Bạn sẽ được chuyển sang ứng dụng Zalo để trao đổi trực tiếp với Empire Garage (0939 658 345)',
-                  style:TextStyle(
-                      fontSize: 15.sp,
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.lightTextColor,
-                      fontFamily: 'SFProDisplay'),
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () => Navigator.pop(context, 'Cancel'),
-                    child: Text(
-                      'Cancel',
-                      style:TextStyle(
-                          fontSize: 15.sp,
-                          fontWeight: FontWeight.w600,
-                          color: AppColors.blueTextColor,
-                          fontFamily: 'SFProDisplay'),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      var openAppResult = await LaunchApp.openApp(
-                        androidPackageName: 'com.zing.zalo',
-                        iosUrlScheme: 'pulsesecure://',
-                        appStoreLink:
-                        'itms-apps://itunes.apple.com/us/app/pulse-secure/id945832041',
-                        // openStore: false
-                      );
-                      print(
-                          'openAppResult => $openAppResult ${openAppResult.runtimeType}');
-                      // Enter thr package name of the App you want to open and for iOS add the URLscheme to the Info.plist file.
-                      // The second arguments decide wether the app redirects PlayStore or AppStore.
-                      // For testing purpose you can enter com.instagram.android
-                    },
-                    child:Text(
-                      'OK',
-                      style:TextStyle(
-                          fontSize: 15.sp,
-                          fontWeight: FontWeight.w600,
-                          color: AppColors.blueTextColor,
-                          fontFamily: 'SFProDisplay'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) => Container(
+                        color: Color(0xff757575), //background color
+                        child: Container(
+                            height: 330.h,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: new BorderRadius.only(
+                                    topLeft: const Radius.circular(40.0),
+                                    topRight: const Radius.circular(40.0)
+                                ),
+                              boxShadow:[
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5), //color of shadow
+                                  spreadRadius: 5, //spread radius
+                                  blurRadius: 7, // blur radius
+                                  offset: Offset(0, 2), // changes position of shadow
+                                  //first paramerter of offset is left-right
+                                  //second parameter is top to down
+                                ),
+                                //you can set more BoxShadow() here
+                              ],
+                            ),
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Container(
+                                  margin: const EdgeInsets.symmetric(horizontal: 12),
+                                  child: Column(
+                                    children: [
+                                      SizedBox(height: 20.h,),
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.circular(20),
+                                        child: SizedBox(
+                                          height: 140.h,
+                                          width: 140.h,
+                                          child: Image.asset("assets/image/icon-logo/zaloicon.png",fit: BoxFit.fill,),
+                                        ),
+                                      ),
+                                      SizedBox(height: 20.h,),
+                                      Text(
+                                        "Truy cập Zalo",
+                                        style: TextStyle(
+                                          fontFamily: 'SFProDisplay',
+                                          fontSize: 18.sp,
+                                          fontWeight: FontWeight.w600,
+                                          color: AppColors.blackTextColor,
+                                        ),
+                                      ),
+                                      SizedBox(height: 20.h,),
+                                      Center(
+                                        child: Text(
+                                          "Chuyển đến ứng dụng Zalo để được trao đổi",
+                                          style: TextStyle(
+                                            fontFamily: 'SFProDisplay',
+                                            fontSize: 14.sp,
+                                            fontWeight: FontWeight.w400,
+                                            color: AppColors.lightTextColor,
+                                          ),
+                                        ),
+                                      ),
+                                      Center(
+                                        child: Text(
+                                          "trực tiếp với Empire Garage",
+                                          style: TextStyle(
+                                            fontFamily: 'SFProDisplay',
+                                            fontSize: 14.sp,
+                                            fontWeight: FontWeight.w400,
+                                            color: AppColors.lightTextColor,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(height: 20.h,),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: <Widget>[
+                                          Expanded(
+                                            child: ElevatedButton(
+                                              onPressed: () async{
+                                                var openAppResult = await LaunchApp.openApp(
+                                                  androidPackageName: 'com.zing.zalo',
+                                                  iosUrlScheme: 'pulsesecure://',
+                                                  appStoreLink:'itms-apps://itunes.apple.com/us/app/pulse-secure/id945832041',
+
+                                                );
+                                                print('openAppResult => $openAppResult ${openAppResult.runtimeType}');
+                                              },
+                                              style: ElevatedButton.styleFrom(
+                                                primary: AppColors.buttonColor,
+                                                fixedSize: Size.fromHeight(50.w),
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius: BorderRadius.circular(36),
+                                                ),
+                                              ),
+                                              child: Text(
+                                                'Chuyển đến Zalo',
+                                                style: TextStyle(
+                                                  fontFamily: 'SFProDisplay',
+                                                  fontSize: 17.sp,
+                                                  fontWeight: FontWeight.w600,
+                                                ),
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                        ),
+                      ));
+            },
             style: ElevatedButton.styleFrom(
               primary: AppColors.buttonColor,
               fixedSize: Size.fromHeight(50.w),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(36),
               ),
             ),
             child: Text(
