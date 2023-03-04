d572643eb109fcc016e00ae1cbd7e5cc84bc89a1
hieutrung0510 <hieutrung0510@gmail.com>
2023-03-04 18:51:55 +0700

    [TrungTDH] Done UserProfileSetting

--- lib/application_layer/screens/user_profile/user_profile_setting.dart
+++ lib/application_layer/screens/user_profile/user_profile_setting.dart
@@ -1,12 +1,18 @@
 import 'package:empiregarage_mobile/application_layer/screens/main_page/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../common/colors.dart';
 
-class UserProfileSettings extends StatelessWidget {
+class UserProfileSettings extends StatefulWidget {
   const UserProfileSettings({super.key});
 
+  @override
+  State<UserProfileSettings> createState() => _UserProfileSettingsState();
+}
+
+class _UserProfileSettingsState extends State<UserProfileSettings> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,46 +38,142 @@ class UserProfileSettings extends StatelessWidget {
             ),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 24),
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: SizedBox(
-              width: 160.w,
-              height: 160.h,
-              child: Stack(
-                children: <Widget>[
-                  SizedBox(
-                    width: 160.w,
-                    height: 160.h,
-                    child: const CircleAvatar(
-                      backgroundImage: AssetImage("assets/image/user-pic/user.png"),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 10,
-                    right: 5,
-                    child: Container(
-                      width: 33.w,
-                      height: 33.h,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: AppColors.buttonColor,
-                        shape: BoxShape.rectangle,
-                      ),
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.edit_rounded,
-                          color: AppColors.whiteButtonColor,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 24),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                  width: 160.w,
+                  height: 160.h,
+                  child: Stack(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 160.w,
+                        height: 160.h,
+                        child: const CircleAvatar(
+                          backgroundImage:
+                              AssetImage("assets/image/user-pic/user.png"),
                         ),
                       ),
+                      Positioned(
+                        bottom: 10,
+                        right: 5,
+                        child: Container(
+                          width: 33.w,
+                          height: 33.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: AppColors.buttonColor,
+                            shape: BoxShape.rectangle,
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.edit_rounded,
+                              color: AppColors.whiteButtonColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: SizedBox(
+                height: 57.h,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5),
+                      child: Text(
+                        "Hồng Phúc",
+                        style: TextStyle(
+                            fontSize: 24.sp,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'SFProDisplay',
+                            color: AppColors.blackTextColor),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "0123456789",
+                        style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'SFProDisplay',
+                            color: AppColors.blackTextColor),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 40.h,),
+            Container(
+              margin:const EdgeInsets.symmetric(horizontal: 24),
+              height: 76.h,
+              child: Column(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      //TODO
+                    },
+                    child: Row(
+                      children: <Widget>[
+                        const Padding(
+                          padding: EdgeInsets.only(right: 24),
+                          child: Icon(FontAwesomeIcons.user,size: 24,),
+                        ),
+                        Text(
+                          "Edit Profile",
+                          style: TextStyle(
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'SFProDisplay',
+                              color: AppColors.blackTextColor),
+                        ),
+                        const Spacer(),
+                        const Icon(Icons.navigate_next,size: 20,color: Colors.black,)
+                  
+                      ],
                     ),
                   ),
+                  const SizedBox(height: 20,),
+                  GestureDetector(
+                    onTap: (){
+                      //TODO
+                    },
+                    child: Row(
+                      children: <Widget>[
+                        const Padding(
+                          padding: EdgeInsets.only(right: 24),
+                          child: Icon(Icons.logout,size: 24,color: Colors.red,),
+                        ),
+                        Text(
+                          "Logout",
+                          style: TextStyle(
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'SFProDisplay',
+                              color: Colors.red),
+                        ),
+                        const Spacer(),
+                        const Icon(Icons.navigate_next,size: 20,color: Colors.red,)
+                  
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
