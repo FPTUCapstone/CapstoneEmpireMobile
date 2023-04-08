141802941bf05b06a1aa834ffae66606c7b3e059
hieutrung0510 <hieutrung0510@gmail.com>
2023-04-08 22:58:55 +0700

    [TrungTDH] Car Management in Profile

--- lib/application_layer/screens/user_profile/car_management.dart
+++ lib/application_layer/screens/user_profile/car_management.dart
@@ -1,4 +1,4 @@
-import 'package:empiregarage_mobile/application_layer/widgets/chose_your_car.dart';
+import 'package:empiregarage_mobile/application_layer/screens/car/add_new_car.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -83,6 +83,29 @@ class _CarManagementState extends State<CarManagement> {
               fontSize: 16,
               color: Colors.black,
             )),
+        actions: [
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.transparent,
+              border: Border.all(
+                color: Colors.white,
+                width: 1.0,
+              ),
+            ),
+            child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    AddNewCar(onAddCar: (int ) {  },),
+                              ));
+                },
+                icon: const Icon(
+                  Icons.add,
+                  color: Colors.black,
+                )),
+          ),
+        ],
       ),
       body: Scaffold(
         backgroundColor: AppColors.green50,
@@ -93,7 +116,7 @@ class _CarManagementState extends State<CarManagement> {
                 itemCount: _listCar.length,
                 itemBuilder: (context, index) => Column(
                   children: [
-                    CarChip(
+                    CarChipManagement(
                       car: _listCar[index],
                       selectedCar: _selectedCar,
                       onSelected: _onCarSelected,
@@ -106,6 +129,93 @@ class _CarManagementState extends State<CarManagement> {
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CarChipManagement extends StatefulWidget {
+  final CarResponseModel car;
+  final int selectedCar;
+  final Function(int) onSelected;
+  const CarChipManagement(
+      {super.key,
+      required this.car,
+      required this.selectedCar,
+      required this.onSelected});
+
+  @override
+  State<CarChipManagement> createState() => _CarChipManagementState();
+}
+
+class _CarChipManagementState extends State<CarChipManagement> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isSelected = widget.car.id == widget.selectedCar;
+    return InkWell(
+      onTap: () {
+        //TODO
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10)),
+        ),
+        child: ListTile(
+          leading: Image.asset(
+            "assets/image/icon-logo/bmw-car-icon.png",
+            height: 50.h,
+            width: 50.w,
+          ),
+          title: Text(
+            widget.car.carBrand,
+            style: TextStyle(
+              fontFamily: 'SFProDisplay',
+              fontSize: 12.sp,
+              fontWeight: FontWeight.w500,
+              color: AppColors.lightTextColor,
+            ),
+          ),
+          subtitle: Align(
+            alignment: Alignment.topLeft,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.car.carLisenceNo,
+                  style: TextStyle(
+                    fontFamily: 'SFProDisplay',
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.blackTextColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 5.h,
+                ),
+                Text(
+                  widget.car.carModel,
+                  style: TextStyle(
+                    fontFamily: 'SFProDisplay',
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.lightTextColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          isThreeLine: true,
         ),
       ),
     );
