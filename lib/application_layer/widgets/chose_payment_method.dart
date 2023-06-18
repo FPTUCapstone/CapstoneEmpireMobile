9e5b80ea8ab6dffb51ad5216f48a1112b8143d4c
minhtn9x <minhtn9x@users.noreply.github.com>
2023-06-18 17:15:13 +0700

    update UI button

--- lib/application_layer/widgets/chose_payment_method.dart
+++ lib/application_layer/widgets/chose_payment_method.dart
@@ -34,175 +34,170 @@ class _ChosePaymentMethodState extends State<ChosePaymentMethod> {
         ),
         shadowColor: Colors.transparent,
       ),
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20.h,
-            ),
-            Row(
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
               children: [
-                Text(
-                  "Phương thức thanh toán",
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 12.sp,
-                    fontWeight: FontWeight.w600,
-                    color: AppColors.blackTextColor,
-                  ),
+                SizedBox(
+                  height: 20.h,
                 ),
-                // const Spacer(),
-                // TextButton(
-                //   onPressed: () {},
-                //   child: Text(
-                //     "Thêm mới",
-                //     style: TextStyle(
-                //       fontFamily: 'Roboto',
-                //       fontSize: 12.sp,
-                //       fontWeight: FontWeight.w500,
-                //       color: AppColors.blueTextColor,
-                //     ),
-                //   ),
-                // ),
-              ],
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 1,
-              itemBuilder: (context, index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10)),
-                    boxShadow: [
-                      BoxShadow(
-                          offset: const Offset(0, 5),
-                          blurRadius: 20,
-                          color: Colors.grey.shade300,
-                          blurStyle: BlurStyle.outer)
-                    ],
-                  ),
-                  child: ListTile(
-                    leading: Image.asset(
-                      "assets/image/icon-logo/vnpay.png",
-                      height: 50.h,
-                      width: 50.w,
-                    ),
-                    title: Text(
-                      "VNPay",
+                Row(
+                  children: [
+                    Text(
+                      "Phương thức thanh toán",
                       style: TextStyle(
                         fontFamily: 'Roboto',
-                        fontSize: 14.sp,
+                        fontSize: 12.sp,
                         fontWeight: FontWeight.w600,
                         color: AppColors.blackTextColor,
                       ),
                     ),
-                    trailing: const Icon(
-                      Icons.radio_button_checked,
-                      color: AppColors.buttonColor,
-                    ),
-                  ),
-                );
-              },
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 1,
-              itemBuilder: (context, index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10)),
-                    boxShadow: [
-                      BoxShadow(
-                          offset: const Offset(0, 5),
-                          blurRadius: 20,
-                          color: Colors.grey.shade300,
-                          blurStyle: BlurStyle.outer)
-                    ],
-                  ),
-                  child: ListTile(
-                    leading: Image.asset(
-                      "assets/image/icon-logo/paypal-icon.png",
-                      height: 50.h,
-                      width: 50.w,
-                    ),
-                    title: Text(
-                      "Paypal",
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.w600,
-                        color: AppColors.blackTextColor,
+                    // const Spacer(),
+                    // TextButton(
+                    //   onPressed: () {},
+                    //   child: Text(
+                    //     "Thêm mới",
+                    //     style: TextStyle(
+                    //       fontFamily: 'Roboto',
+                    //       fontSize: 12.sp,
+                    //       fontWeight: FontWeight.w500,
+                    //       color: AppColors.blueTextColor,
+                    //     ),
+                    //   ),
+                    // ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 1,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                        boxShadow: [
+                          BoxShadow(
+                              offset: const Offset(0, 5),
+                              blurRadius: 20,
+                              color: Colors.grey.shade300,
+                              blurStyle: BlurStyle.outer)
+                        ],
                       ),
-                    ),
-                    trailing: const Icon(
-                      Icons.radio_button_off,
-                      color: AppColors.grey600,
-                    ),
-                  ),
-                );
-              },
-            ),
-            SizedBox(
-              height: 5.h,
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20.sp),
-                  child: Row(
-                    //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    //crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      //Divider(thickness: 2, color: Colors.red),
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            widget.excute();
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: AppColors.buttonColor,
-                            fixedSize: Size.fromHeight(50.w),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                          ),
-                          child: Text(
-                            'Xác nhận',
-                            style: TextStyle(
-                              fontFamily: 'Roboto',
-                              fontSize: 14.sp,
-                              fontWeight: FontWeight.w600,
-                            ),
+                      child: ListTile(
+                        leading: Image.asset(
+                          "assets/image/icon-logo/vnpay.png",
+                          height: 50.h,
+                          width: 50.w,
+                        ),
+                        title: Text(
+                          "VNPay",
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.blackTextColor,
                           ),
                         ),
-                      )
-                    ],
+                        trailing: const Icon(
+                          Icons.radio_button_checked,
+                          color: AppColors.buttonColor,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 1,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                        boxShadow: [
+                          BoxShadow(
+                              offset: const Offset(0, 5),
+                              blurRadius: 20,
+                              color: Colors.grey.shade300,
+                              blurStyle: BlurStyle.outer)
+                        ],
+                      ),
+                      child: ListTile(
+                        leading: Image.asset(
+                          "assets/image/icon-logo/paypal-icon.png",
+                          height: 50.h,
+                          width: 50.w,
+                        ),
+                        title: Text(
+                          "Paypal",
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.blackTextColor,
+                          ),
+                        ),
+                        trailing: const Icon(
+                          Icons.radio_button_off,
+                          color: AppColors.grey600,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 5.h,
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          Divider(thickness: 2),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10.sp),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  widget.excute();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.buttonColor,
+                  fixedSize: Size.fromHeight(50.w),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: Text(
+                  'Xác nhận',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
