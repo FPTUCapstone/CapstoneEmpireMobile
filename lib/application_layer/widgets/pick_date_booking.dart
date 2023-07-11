85f5fcc000d1068a7ad2bae177e38b7e1bed0a54
minhtn9x <minhtn9x@users.noreply.github.com>
2023-07-11 14:32:02 +0700

    fix pick date booking

--- lib/application_layer/widgets/pick_date_booking.dart
+++ lib/application_layer/widgets/pick_date_booking.dart
@@ -27,19 +27,22 @@ class _PickDateBookingState extends State<PickDateBooking> {
     var workload = await BookingService().getMinWorkload();
     DateTime dateCanBook = workload!.intendedFinishTime;
     int inactiveDates = dateCanBook.subtract(Duration(days: 1)).compareTo(DateTime.now());
-    if(inactiveDates < 0){
-      inactiveDates = 0;
-    }
     List<DateTime> list = [];
-    for(int i = 0; i <= inactiveDates; i++){
-      list.add(DateTime.now().add(Duration(days: i)));
-    }
-    if (mounted) {
+    if(inactiveDates >= 0){
+      for(int i = 0; i <= inactiveDates; i++){
+        list.add(DateTime.now().add(Duration(days: i)));
+      }
       setState(() {
         _dateCanBook = dateCanBook;
         _loading = true;
         _list = list;
         _selectedDate = list[list.length -1].add(Duration(days: 1));
+      });
+    }
+    if(inactiveDates < 0){
+      setState(() {
+        _selectedDate = dateCanBook;
+        _loading = true;
       });
     }
     return dateCanBook;
